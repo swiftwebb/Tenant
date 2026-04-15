06c28875aa08165ef0d7332154ba45c565657e85
swiftwebb <swiftwebbb@gmail.com>
2026-04-16 00:12:34 +0100

    fix

--- ecom/views.py
+++ ecom/views.py
@@ -559,6 +559,7 @@ def remove_all(request):
 @ratelimit(key='ip', rate='10/m', block=True)
 def checkout(request):
     tenant = request.tenant
+    from ecom.models import *
 
     import cloudinary
 
