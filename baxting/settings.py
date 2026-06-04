3a9f8fc53db935082b020b5f33bb4cc4dccf4ae6
swiftwebb <swiftwebbb@gmail.com>
2026-06-04 22:31:33 +0100

    first commit

--- baxting/settings.py
+++ baxting/settings.py
@@ -16,11 +16,6 @@ project = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')  
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# if project==True:
-#     DEBUG = False
-# else:
-#     DEBUG = True
 
 DEBUG = False
 
